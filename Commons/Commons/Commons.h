010b6cd9764f958168f3e17398c7f4f6016db081
josevictor1 <josevictorpatos@gmail.com>
2019-09-21 12:47:23 -0300

    Create Commons target and fixing typing error

---
+++ Commons/Commons/Commons.h
@@ -0,0 +1,19 @@
+//
+//  Commons.h
+//  Commons
+//
+//  Created by José Victor Pereira Costa on 21/09/19.
+//  Copyright © 2019 José Victor Pereira Costa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Commons.
+FOUNDATION_EXPORT double CommonsVersionNumber;
+
+//! Project version string for Commons.
+FOUNDATION_EXPORT const unsigned char CommonsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Commons/PublicHeader.h>
+
+
