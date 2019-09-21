cad077ca2c628873475f46b8e769249a7a415f0f
josevictor1 <josevictorpatos@gmail.com>
2019-09-21 11:43:05 -0300

    Organizing project folders

---
+++ QuizApp/AppDelegate/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  QuizApp
+//
+//  Created by José Victor Pereira Costa on 21/09/19.
+//  Copyright © 2019 José Victor Pereira Costa. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
